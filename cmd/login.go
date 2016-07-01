733c65fd716d07c4feed43df09e229efa90ab00c
Ben Tranter <bwtrante@lakeheadu.ca>
2016-07-01 14:09:18 -0400

    Verify account against who-am-i endpoint

--- cmd/login.go
+++ cmd/login.go
@@ -18,9 +18,12 @@ package cmd
 import (
 	"encoding/base64"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
 
+	// "github.com/docker/docker-credential-helpers/credentials"
+	// "github.com/docker/docker-credential-helpers/osxkeychain"
 	"github.com/spf13/cobra"
 )
 
@@ -29,14 +32,15 @@ var loginCmd = &cobra.Command{
 	Use:   "login",
 	Short: "Login to your Harvest account via the CLI",
 	Long:  `Login to your Harvest account by supplying a username and password.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("login called")
-	},
+	Run:   login,
 }
 
 func init() {
 	RootCmd.AddCommand(loginCmd)
 
+	loginCmd.PersistentFlags().String("email", "", "the email associated with your Harvest account")
+	loginCmd.PersistentFlags().String("password", "", "the password associated with your Harvest account")
+	loginCmd.PersistentFlags().String("org", "", "the organization you belong to on Harvest")
 	// Here you will define your flags and configuration settings.
 	// We need the --user, --email, --password, and --org flags
 
@@ -48,18 +52,36 @@ func init() {
 
 }
 
+// login accepts a username and password, checking to make sure that they match
+// a valid Harvest account. If they do, the username and password are stored in
+// the system's secure vault (currently only keychain on OS X).
 func login(cmd *cobra.Command, args []string) {
-	req, err := http.NewRequest("POST", "https://harvest.com/api", nil)
+	id, _ := cmd.Flags().GetString("email")
+	secret, _ := cmd.Flags().GetString("password")
+	org, _ := cmd.Flags().GetString("org")
+	cred := base64.StdEncoding.EncodeToString([]byte(id + ":" + secret))
+
+	req, err := http.NewRequest("GET", "https://"+org+".harvestapp.com/account/who_am_i", nil)
 	if err != nil {
 		fmt.Printf("Error creating new HTTP request: %s\n", err.Error())
 		os.Exit(-1)
 		return
 	}
+	req.Header.Set("Authorization", "Basic "+cred)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
+
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		fmt.Printf("Error during HTTP request: %s\n", err.Error())
 		os.Exit(-1)
 		return
 	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("Error reading who am i response: %s\n", err.Error())
+	}
+	defer resp.Body.Close()
 
+	fmt.Printf("Response:\n\n%s\n", body)
 }
